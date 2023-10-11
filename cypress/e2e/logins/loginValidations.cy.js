77a9e0afb9085a2d9321dea9c93b79cbabd4d4c3
Jefferson Willian <jefferson.lsilva@me.com.br>
2023-10-11 00:17:24 -0300

    feat: Finalização dos cenários de logins

---
+++ cypress/e2e/logins/loginValidations.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+
+describe('Tests for Logins Validations', () => {
+  beforeEach(() => {
+
+    cy.visit('https://seubarriga.wcaquino.me/login')
+
+    cy.gui_validatePage()
+
+  })
+
+  it('Create User Successfully', () => {
+
+    cy.gui_acessNewUserPage()
+
+    cy.gui_createNewUserSuccessfully()
+
+    cy.gui_validateAlertSuccess()
+
+  })
+
+  it('Validate Unregistered User', () => {
+
+    cy.gui_loginInvalidUser()
+
+  })
+
+  it('Login successfully', () => {
+    
+    cy.gui_loginSuccessfully()
+  })
+})
---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,103 @@
+import { fakerPT_BR } from '@faker-js/faker';
+import Data from '../fixtures/userdate.json'
+
+
+Cypress.Commands.add('gui_createNewUserSuccessfully', () => {
+
+   const userName = fakerPT_BR.internet.userName()
+   const userEmail = fakerPT_BR.internet.email({ firstName: userName })
+   const userPassword = fakerPT_BR.internet.password()
+
+
+   cy.get('#nome')
+      .should('be.visible')
+      .type(userName)
+
+   cy.get('#email')
+      .should('be.visible')
+      .type(userEmail)
+
+   cy.get('#senha')
+      .should('be.visible')
+      .type(userPassword, { log: false })
+
+   cy.gui_clickRegisterButton()
+
+})
+
+Cypress.Commands.add('gui_validatePage', () => {
+   cy.get('.navbar-brand')
+      .should('be.visible')
+      .contains('Seu Barriga')
+})
+
+Cypress.Commands.add('gui_clickRegisterButton', () => {
+   cy.get('input[value=Cadastrar]')
+      .should('be.visible')
+      .click()
+
+})
+
+Cypress.Commands.add('gui_acessNewUserPage', () => {
+   cy.get('li a')
+      .should('be.visible')
+      .contains('Novo usuário?')
+      .click()
+})
+
+Cypress.Commands.add('gui_validateAlertSuccess', () => {
+   cy.get('.alert-success')
+      .should('be.visible')
+})
+
+//-----------------------------------//
+
+Cypress.Commands.add('gui_loginInvalidUser', () => {
+
+   const userEmail = fakerPT_BR.internet.email()
+   const userPassword = fakerPT_BR.internet.password()
+
+   cy.get('#email')
+      .should('be.visible')
+      .type(userEmail)
+
+   cy.get('#senha')
+      .should('be.visible')
+      .type(userPassword, { log: false })
+
+   cy.gui_enterButton()
+
+   cy.gui_dangerMessage()
+})
+
+Cypress.Commands.add('gui_enterButton', () => {
+   cy.get('button')
+      .contains('Entrar')
+      .click()
+})
+
+Cypress.Commands.add('gui_dangerMessage', () => {
+   cy.get('.alert-danger')
+      .should('be.visible')
+})
+
+
+//===================================================================//
+Cypress.Commands.add('gui_loginSuccessfully', () => {
+
+   cy.get('#email')
+      .should('be.visible')
+      .type(Data.email)
+
+   cy.get('#senha')
+      .should('be.visible')
+      .type(Cypress.env('senha'), {log: false})
+
+   cy.gui_enterButton()
+
+   cy.gui_validateAlertSuccess()
+
+})
+
+
+
